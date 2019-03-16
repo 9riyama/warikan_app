ae509b5ccd62d12a46ebb283942db393a0076c16
Yusuke Kuriyama <selmelo3@yahoo.co.jp>
2019-03-16 17:31:48 +0000

    category_deta_change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190316025602) do
+ActiveRecord::Schema.define(version: 20190316165855) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
