cc1fdb01837356d58f30eb70409465777ae90d06
Yusuke Kuriyama <selmelo3@yahoo.co.jp>
2019-03-27 14:18:37 +0000

    models_add_validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
-  validates :content, {presence: true, length: {maximum: 140}}
-  validates :user_id, {presence: true}
   
+  validates :user_id, presence: true
+  validates :payment, presence: true
+  validates :content, length: {maximum: 140}
+
   belongs_to :user
   belongs_to :category
 
