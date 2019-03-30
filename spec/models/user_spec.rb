dd347c770e5d26310409f41a9b9a02d475eb2988
Yusuke Kuriyama <selmelo3@yahoo.co.jp>
2019-03-30 05:09:35 +0000

    user_spec_add_length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  #名前、メール、パスワードがあれば有効な状態であること
-  it "is valid with a name, email, and password" do
+
+  it "名前、メール、パスワードがあれば有効な状態であること" do
     user = User.new(
         name: "テスト太郎",
         email: "tester@example.com" ,
@@ -12,24 +11,25 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
   
-  
-  # 名前がなければ無効な状態であること
-  it "is invalid without a first name" do
+  it "名前がなければ無効な状態であること" do
     user = User.new(name: nil)
     user.valid?
     expect(user.errors[:name]).to include("を入力してください")
   end
-  
-  # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address" do
-   user = User.new(email: nil)
-   user.valid?
-   expect(user.errors[:email]).to include("を入力してください")
+
+  it "メールアドレスがなければ無効な状態であること" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください")
   end
   
-  
-  # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address" do
+  it "パスワードがなければ無効な状態であること" do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("を入力してください")
+  end
+
+  it "重複したメールアドレスなら無効な状態であること" do
     User.create(
         name: "テスト太郎",
         email: "tester@example.com" ,
@@ -44,14 +44,29 @@ RSpec.describe User, type: :model do
     expect(user.errors[:email]).to include("はすでに存在します")
   end
   
-  
-  
-  # パスワードがなければ無効な状態であること
-  it "is invalid without an password" do
-     user = User.new(password: nil)
-   user.valid?
-   expect(user.errors[:password]).to include("を入力してください")
+  it "名前が51文字以上で無効な状態であること" do
+    user = User.new(name: "a" * 51)
+    user.valid?
+    expect(user.errors[:name]).to include("は50文字以内で入力してください")
   end
   
+  it "メールアドレスが256文字以上で無効な状態であること" do
+    user = User.new(email: "a" * 244 + "@example.com")
+    user.valid?
+    expect(user.errors[:email]).to include("は255文字以内で入力してください")
+  end
   
+  it "パスワードが5文字以内で無効な状態であること" do
+    user = User.new
+    user.password = user.password_confirmation = "a" * 5
+    user.valid?
+    expect(user.errors[:password]).to include("は6文字以上で入力してください")
+  end
+  
+  it "パスワードが6文字以上の空文字が入力された場合無効な状態であること" do
+    user = User.new
+    user.password = user.password_confirmation = " " * 6
+    user.valid?
+    expect(user.errors[:password]).to include("を入力してください")
+  end
 end
